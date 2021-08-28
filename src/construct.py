f817fa025d3b398b8f81311878cf277b78138263
Sam Spekreijse <speksam@gmail.com>
2021-08-28 16:01:52 +1200

    Add Aoe classes.

--- src/construct.py
+++ src/construct.py
@@ -15,6 +15,22 @@ class Construct:
 		#TODO: Add other collision types, like 50% chance of noticing for pit traps. 
 
 
+class Aoe(Construct):
+
+	def __init__(self, colour, position, effectRange):
+		Construct.__init__(self, colour)
+		self.position = position
+		self.effectRange = effectRange
+
+	def isInRange(self, testPosition):
+		dx = self.position[0] - testPosition[0]
+		dy = self.position[1] - testPosition[1]
+		return self.effectRange**2 >= dx**2 + dy**2
+
+
+
+
+
 class Fence(Construct):
 
 	def __init__(self):
@@ -27,9 +43,23 @@ class PitTrap(Construct):
 		Construct.__init__(self, CONSTRUCT_COLOURS[PitTrap])
 
 
+class TownCentre(Aoe):
+	def __init__(self, position):
+		Aoe.__init__(CONSRUCT_COLOUR[TownCentre], position, 7)
+	
+
+class DungHeap(Aoe):
+	def __init__(self, position):
+		Aoe.__init__(CONSRUCT_COLOUR[DungHeap], position, 2)
+
+
+
+
 CONSTRUCT_COLOURS = {
 	Fence: (128, 64, 16), 
-	PitTrap: (255, 0, 0)
+	PitTrap: (255, 0, 0),
+	TownCentre: (255, 180, 0), 
+	DungHeap: (70, 80, 0)
 }
 
 CONSTRUCT_FROMID = {
